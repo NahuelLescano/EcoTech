a9ca5402c0136feeda4125f821fb181662209db9
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-29 22:43:29 -0300

    fix: muevo el archivo dentro de src/database

--- backend/src/database/init.sql
+++ backend/src/database/init.sql
@@ -1,17 +1,11 @@
--- Se usa CamelCase para el nombre de las tablas y snake_case para los nombres de las columnas.
-
--- Tabla para almacenar información sobre los contenedores de residuos en los hubs de reciclaje.
-
 CREATE TABLE ContenedoresHub (
     id SERIAL PRIMARY KEY,
     ubicacion_barrio VARCHAR(100) NOT NULL,
     tipo_residuo_permitido VARCHAR(50) NOT NULL,
     capacidad_maxima_kg INT NOT NULL,
     carga_actual_kg INT NOT NULL,
-    estado_llenado VARCHAR(20) NOT NULL DEFAULT 'Vacante',
+    estado_llenado VARCHAR(20) NOT NULL DEFAULT 'Vacante'
 );
-
--- Tabla para almacenar información sobre los depósitos de residuos realizados por los vecinos en los contenedores.
 
 CREATE TABLE DepositosResiduos (
     id SERIAL PRIMARY KEY,
@@ -19,10 +13,8 @@ CREATE TABLE DepositosResiduos (
     dni_vecino VARCHAR(20) NOT NULL,
     peso_ingresado_kg INT NOT NULL,
     puntos_otorgados INT NOT NULL,
-    fecha_deposito TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    fecha_deposito TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
--- Tabla para almacenar información sobre las órdenes de retiro de residuos de los contenedores.
 
 CREATE TABLE OrdenesRetiros (
     id SERIAL PRIMARY KEY,
@@ -30,7 +22,5 @@ CREATE TABLE OrdenesRetiros (
     empresa_recolectora VARCHAR(100) NOT NULL,
     patente_camion VARCHAR(20) NOT NULL,
     fecha_programada TIMESTAMP NOT NULL,
-    estado_orden VARCHAR(20) NOT NULL DEFAULT 'Pendiente',
+    estado_orden VARCHAR(20) NOT NULL DEFAULT 'Pendiente'
 );
-
-
