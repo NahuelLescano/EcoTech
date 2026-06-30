ce2ee59f11a4f9abb39c3cf415184f3b43e8a124
Mateo Appio <mateedev@gmail.com>
2026-06-30 12:13:51 -0300

    Agrego express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello, World!");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const corsOptions = {
+  origin: "*", // Replace with your domain
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+const port = 8000;
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(port, () => {
+  console.log(`EcoTechAPI listening on port ${port}`);
+});
