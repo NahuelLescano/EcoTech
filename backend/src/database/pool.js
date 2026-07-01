ed09983a3fdb7ea28d7a2ec1b035a66d12c19c72
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-30 22:08:55 -0300

    fix: solo para mantener la consistencia

--- backend/src/database/pool.js
+++ backend/src/database/pool.js
@@ -1,10 +1,9 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
-
