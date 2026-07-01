b1f99dc7638c85d97647fcf53d56eff4958ac5f8
Mateo Appio <mateedev@gmail.com>
2026-07-01 19:39:23 -0300

    Endpoint sin validaciones, actualiza tabla contenedorHub y crea ordenRetiro en caso de ser necesario, cambio en init.sql

--- backend/src/controller/depositos.controller.js
+++ backend/src/controller/depositos.controller.js
@@ -1,4 +1,9 @@
-import { createDepositoResiduo } from "../database/depositosResiduos.js";
+import {
+  createDepositoResiduo,
+  createOrdenRetiro,
+  getContenedorById,
+  updateContenedorCargaYEstado,
+} from "../database/depositosResiduos.js";
 // Equivalencias para puntos de materiales
 // Los puntos de cada material equivalen a un kg
 
@@ -16,15 +21,41 @@ export const registrarDeposito = async (req, res) => {
 
     const puntosOtorgados = pesoIngresado * materiales[tipoResiduo];
 
-    const created = await createDepositoResiduo(
+    // Sumo a la carga actual del contendor el peso nuevo ingresado en el deposito por el vecino
+    const contenedor = await getContenedorById(contenedorId);
+
+    const nuevaCarga = contenedor.carga_actual_kg + pesoIngresado;
+
+    const porcentajeMaximo = contenedor.capacidad_maxima_kg * 0.9;
+
+    let nuevoEstado = "Vacante";
+
+    let alertaLleno = false;
+
+    // Separo en casos
+    if (nuevaCarga >= porcentajeMaximo) {
+      nuevoEstado = "Lleno";
+      alertaLleno = true;
+    }
+
+    await createDepositoResiduo(
       contenedorId,
       dniVecino,
       pesoIngresado,
       puntosOtorgados,
     );
 
+    await updateContenedorCargaYEstado(nuevaCarga, nuevoEstado, contenedorId);
+
+    if (alertaLleno) {
+      await createOrdenRetiro(contenedorId);
+    }
+
     // Si todo salio bien
-    res.status(201).json({ message: "Deposito registrado" });
+    res.status(201).json({
+      message: "Deposito registrado con exito",
+      puntosOtorgados, // Devuelvo los puntos al front
+    });
   } catch (error) {
     // Si algo falla, atrapamos el error
     res.status(500).json({
--- backend/src/database/depositosResiduos.js
+++ backend/src/database/depositosResiduos.js
@@ -12,3 +12,30 @@ export async function createDepositoResiduo(
   );
   return res.rowCount == 1;
 }
+
+export async function getContenedorById(id) {
+  const res = await pool.query(
+    "SELECT capacidad_maxima_kg, carga_actual_kg FROM ContenedoresHub where id = $1 LIMIT 1",
+    [id],
+  );
+
+  return res.rows[0];
+}
+
+export async function updateContenedorCargaYEstado(
+  nuevaCarga,
+  nuevoEstado,
+  id,
+) {
+  const res = await pool.query(
+    "UPDATE ContenedoresHub SET carga_actual_kg = $1, estado_llenado = $2 where id = $3",
+    [nuevaCarga, nuevoEstado, id],
+  );
+}
+
+export async function createOrdenRetiro(contenedorId) {
+  const res = await pool.query(
+    "INSERT INTO OrdenesRetiros (contenedor_id) values ($1)",
+    [contenedorId],
+  );
+}
