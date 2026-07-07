e746cfd17d2c73bcac849ee144f8c3f67ff77217
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-07-07 00:02:35 -0300

    feat: maquetado del formulario del deposito de la Terminal de Vecinos y conecto con POST /api/depositos

--- frontend/js/terminal.js
+++ frontend/js/terminal.js
@@ -1,1 +1,46 @@
-console.log("Terminal del Vecino");
+const API_URL = "http://localhost:8000/api/depositos";
+
+const form = document.getElementById("form-deposito");
+const notification = document.getElementById("notificacion");
+const notificationText = document.getElementById("notificacion-texto");
+const btnDelete = notification.querySelector(".delete");
+
+btnDelete.addEventListener("click", () => {
+  notification.classList.add("is-hidden");
+});
+
+const mostrarNotificacion = (mensaje, tipo) => {
+  notificationText.textContent = mensaje;
+  notification.className = `notification ${tipo}`;
+};
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const body = {
+    contenedorId: Number(document.getElementById("contenedorId").value),
+    dniVecino: document.getElementById("dniVecino").value.trim(),
+    tipoResiduo: document.getElementById("tipoResiduo").value,
+    pesoIngresado: Number(document.getElementById("pesoIngresado").value),
+  }
+
+  try {
+    const res = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body)
+    });
+
+    const data = await res.json();
+    if (!res.ok) {
+      mostrarNotificacion(data.message ?? "Error al registrar un deposito", "is-danger");
+      return;
+    }
+
+    mostrarNotificacion(`Depósito registrado correctamente. Ganaste ${data.puntosOtorgados} puntos`, "is-success");
+  } catch (err) {
+    mostrarNotificacion("Error al registrar el depósito", "is-danger");
+  }
+});
