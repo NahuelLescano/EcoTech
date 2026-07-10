bab5c32a5b0833690358db9b359d4407832da4c0
Mateo Appio <mateedev@gmail.com>
2026-07-10 11:17:16 -0300

    Creo el Dockerfile y el docker compose, y modifico el pool de conexion a la db

--- backend/src/database/pool.js
+++ backend/src/database/pool.js
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
